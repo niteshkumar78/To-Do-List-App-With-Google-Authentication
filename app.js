3175ea4bbb215ca2153d3e9ceab16c9cb5a2e667
Nitesh Kumar <niteshkumartg@gmail.com>
2021-05-08 21:39:51 +0530

    as

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ app.use(passport.session());
 
 mongoose.connect("mongodb+srv://admin-nitesh:Test@123@cluster0.af934.mongodb.net/todolistDB", { useNewUrlParser: true ,useUnifiedTopology: true });
 
-
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 // const itemSchema= mongoose.Schema({
 //     name: String
 // });
