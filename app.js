1f132f9774762df29a5e1288ef1d435f93ae2236
Nitesh Kumar <niteshkumartg@gmail.com>
2021-05-09 13:26:40 +0530

    slknds

--- app.js
+++ app.js
@@ -104,11 +104,13 @@ app.use(express.static("assets"));
 
 
 app.get('/', function(req, res){
-    if(req.isAuthenticated()){
-        res.redirect('/list');
-    } else{
-        res.render('home');
-    }
+    // if(req.isAuthenticated()){
+    //     res.redirect('/list');
+    // } else{
+    //     res.render('home');
+    // }
+
+    res.render('home');
     
 });
 
@@ -128,22 +130,23 @@ app.get('/auth/google',
   });
 
 app.get('/login', function(req, res){
-    if(req.isAuthenticated()){
-        res.redirect('/list');
-    } else{
-        res.render('login');
-    }
+    // if(req.isAuthenticated()){
+    //     res.redirect('/list');
+    // } else{
+    //     res.render('login');
+    // }
+    res.render('login');
    
 });
 
 
 app.get('/register', function(req, res){
-    if(req.isAuthenticated()){
-        res.redirect('/list');
-    } else{
-        res.render('register');
-    }
-   
+    // if(req.isAuthenticated()){
+    //     res.redirect('/list');
+    // } else{
+    //     res.render('register');
+    // }
+    res.render('register');
 });
 
 app.post('/register', function(req, res){
